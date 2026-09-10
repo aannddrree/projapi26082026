14e5c5e1a7fbcbce26b182820a1303f85a1341d3
André Luiz da Silva <andreluizdasilva@MacBook-Air-de-Andre.local>
2026-09-09 21:05:35 -0300

    v2

--- src/main/java/org/example/controller/AlunoController.java
+++ src/main/java/org/example/controller/AlunoController.java
@@ -1,4 +1,26 @@
 package org.example.controller;
 
+import org.example.model.Aluno;
+import org.example.service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
 public class AlunoController {
+    private static final String ENDPOINT = "uniara/api/v1/aluno";
+
+    @Autowired
+    private AlunoService alunoService;
+
+    @PostMapping(ENDPOINT)
+    public Aluno save(Aluno aluno){
+        return alunoService.save(aluno);
+    }
+    @GetMapping(ENDPOINT)
+    public List<Aluno> findAll(){
+        return alunoService.findAll();
+    }
 }
